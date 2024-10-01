004d7812ea0da5e8781c5c3a0f168efc3ad17fa7
MrR007 <qorri24j@gmail.com>
2024-10-01 12:14:56 +0700

    add file logo

--- scripts/logo.py
+++ scripts/logo.py
@@ -1,13 +1,26 @@
 import colorama
+import os
+
 from colorama import Fore
 
-colorama.init(autoreset=True)
-logo = r"""
-  ____                                _ ____    _   _ 
- |  _ \ _ __ ___  _   ____   __      | ||    \ | \ | |
- | |_) | '__/ _ \\ \ / /\ \ / / ____ | || |\  ||  \| |
- |  __/| | | (_) |> x <  \ v / |____|| || |/  /| |\  |
- |_|   |_|  \___//_/ \_\  / /        |_||____/ |_| \_|
- .:: Dev : @qorri-di ::. /_/  .:: ver : 0.0.1-Beta ::.                     
-"""
-print(Fore.WHITE + logo)
+def clear():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def pattern():
+    colorama.init(autoreset=True)
+    log = r"""
+    ____                                _ ____    _   _ 
+    |  _ \ _ __ ___  _   ____   __      | ||    \ | \ | |
+    | |_) | '__/ _ \\ \ / /\ \ / / ____ | || |\  ||  \| |
+    |  __/| | | (_) |> x <  \ v / |____|| || |/  /| |\  |
+    |_|   |_|  \___//_/ \_\  / /        |_||____/ |_| \_|
+    .:: Dev : @qorri-di ::. /_/  .:: ver : 0.0.1-Beta ::.                     
+    """
+    print(Fore.WHITE + log)
+
+def logo():
+    clear()
+    pattern()
