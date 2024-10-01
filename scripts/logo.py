f2106d81a8af8470efb9ed697e05833c0c20dfc9
MrR007 <qorri24j@gmail.com>
2024-10-01 11:44:00 +0700

    add file logo

---
+++ scripts/logo.py
@@ -0,0 +1,13 @@
+import colorama
+from colorama import Fore
+
+colorama.init(autoreset=True)
+logo = r"""
+  ____                                _ ____    _   _ 
+ |  _ \ _ __ ___  _   ____   __      | ||    \ | \ | |
+ | |_) | '__/ _ \\ \ / /\ \ / / ____ | || |\  ||  \| |
+ |  __/| | | (_) |> x <  \ v / |____|| || |/  /| |\  |
+ |_|   |_|  \___//_/ \_\  / /        |_||____/ |_| \_|
+ .:: Dev : @qorri-di ::. /_/  .:: ver : 0.0.1-Beta ::.                     
+"""
+print(Fore.WHITE + logo)
